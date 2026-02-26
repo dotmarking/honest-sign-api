8d9416f20cc6c4594ae16ae82b69899d109d4169
Tr1gEr <slava_kovalev_05@mail.ru>
2026-02-26 16:30:22 +0300

    u

--- src/card/api-card.module.ts
+++ src/card/api-card.module.ts
@@ -1,23 +1,6 @@
 import { HttpModule } from '@nestjs/axios';
-import { DynamicModule, Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CardApiService } from './api-card.service';
 
-@Global()
-@Module({})
-export class CardApiModule {
-    static forRoot(): DynamicModule {
-        return {
-            module: CardApiModule,
-            imports: [
-                HttpModule.registerAsync({
-                    useFactory: () => ({
-                        baseURL: 'https://апи.национальный-каталог.рф',
-                        timeout: 10000,
-                    }),
-                }),
-            ],
-            providers: [CardApiService],
-            exports: [CardApiService, HttpModule],
-        };
-    }
-}
+@Module({ imports: [HttpModule], providers: [CardApiService], exports: [CardApiService] })
+export class CardApiModule {}
