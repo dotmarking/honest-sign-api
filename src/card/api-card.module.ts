ff0b9e534a85a164d969546723189d74f624ab87
Tr1gEr <slava_kovalev_05@mail.ru>
2026-02-26 16:34:29 +0300

    u

--- src/card/api-card.module.ts
+++ src/card/api-card.module.ts
@@ -2,5 +2,5 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { CardApiService } from './api-card.service';
 
-@Module({ imports: [HttpModule], providers: [CardApiService], exports: [CardApiService] })
+@Module({ imports: [], providers: [CardApiService], exports: [CardApiService] })
 export class CardApiModule {}
