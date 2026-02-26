4ba7d6b8303ab1d108ec3f513469ebf8e94c42a6
Tr1gEr <slava_kovalev_05@mail.ru>
2026-02-26 16:58:38 +0300

    update

--- src/card/api-card.module.ts
+++ src/card/api-card.module.ts
@@ -2,5 +2,13 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { CardApiService } from './api-card.service';
 
-@Module({ imports: [HttpModule], providers: [CardApiService], exports: [CardApiService] })
+@Module({
+    imports: [
+        HttpModule.register({
+            baseURL: 'https://апи.национальный-каталог.рф',
+        }),
+    ],
+    providers: [CardApiService],
+    exports: [CardApiService],
+})
 export class CardApiModule {}
