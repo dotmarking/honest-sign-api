6232301eb637193c596bd9ffca5976b0ca3d593f
Tr1gEr <slava_kovalev_05@mail.ru>
2026-02-28 16:33:43 +0300

    add index.ts for user module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './auth';
 export * from './card';
 export * from './cis';
+export * from './user';
 export * from './honest-sign.module';
---
+++ src/user/index.ts
@@ -0,0 +1,2 @@
+export * from './honest-sign-user.module';
+export * from './honest-sign-user.service';
