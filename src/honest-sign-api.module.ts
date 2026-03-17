8b047a03baf153fc5ce9654bca1254ab13b0bfc4
Tr1gEr <slava_kovalev_05@mail.ru>
2026-03-17 20:42:43 +0300

    Зарегестрирован модуль bff

--- src/honest-sign-api.module.ts
+++ src/honest-sign-api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TrueApiModule } from './true-api/true-api.module';
 import { SuzApiModule } from './suz-api';
+import { BffApiModule } from './bff-api/bff-api.module';
 
 @Module({
-    imports: [TrueApiModule, SuzApiModule],
-    exports: [TrueApiModule, SuzApiModule],
+    imports: [TrueApiModule, SuzApiModule, BffApiModule],
+    exports: [TrueApiModule, SuzApiModule, BffApiModule],
 })
 export class HonestSignAPIModule {}
